c4aa7d6766aed3216e4f911030edc09c0c90ae6e
saroar <saroar9@gmail.com>
2020-12-03 14:37:14 +0300

    remove create and get register users from array of contacts
    no more contacts on server side

--- Sources/App/Controllers/ContactController.swift
+++ Sources/App/Controllers/ContactController.swift
@@ -12,67 +12,22 @@ import Fluent
 
 extension ContactController: RouteCollection {
   func boot(routes: RoutesBuilder) throws {
-    routes.postBigFile(use: create)
+    routes.postBigFile(use: getRegisterContacts)
   }
 }
 
 class ContactController {
   
-  func create(_ req: Request) throws -> EventLoopFuture<[Contact.ReqRes]> {
+  func getRegisterContacts(_ req: Request) throws -> EventLoopFuture<[User.Response]> {
     if req.loggedIn == false { throw Abort(.unauthorized) }
     
-    let inputData = try req.content.decode([Contact.ReqRes].self)
-    
-    return Contact.query(on: req.db)
-      .filter(\.$user.$id == req.payload.userId)
-      .all()
-      .flatMap { contactsRes in
-        let contacts = contactsRes.map { $0.response }
-        
-        let setOriginal = Set(inputData)
-        let setServerContacts = Set(contacts)
-        let uniqServer = setOriginal.subtracting(setServerContacts)
-        let uniqOrg = setServerContacts.subtracting(setOriginal)
-        
-        if uniqServer.isEmpty && uniqOrg.isEmpty {
-          return Contact.query(on: req.db)
-            .filter(\.$user.$id == req.payload.userId)
-            .all()
-            .map { return $0.map { return $0.response } }
-        }
-        
-        let newContacts = uniqServer.isEmpty ? uniqOrg : uniqServer
-        
-        let results = newContacts.compactMap { (contact: Contact.ReqRes) -> EventLoopFuture<Contact.ReqRes> in
-          
-          return User.query(on: req.db)
-            .with(\.$attachments)
-            .filter(\.$phoneNumber == contact.phoneNumber)
-            .all()
-            .flatMap { users -> EventLoopFuture<Contact.ReqRes> in
-              
-              let user = users.first == nil ? User(phoneNumber: "") : users.first!
-              
-              let lastImageURLString = user.phoneNumber != "" && user.attachments.last != nil ? user.attachments.last!.imageUrlString : ""
-              
-              let contact = Contact(
-                phoneNumber: contact.phoneNumber,
-                identifier: contact.identifier ?? "",
-                fullName: contact.fullName,
-                avatar: lastImageURLString,
-                isRegister: user.phoneNumber == contact.phoneNumber,
-                userId: contact.userId
-              )
-              
-              return contact.save(on: req.db).transform(to: contact.response) //.map { _ in contact }
-              
-            }
-          
-        }
-        
-        return results.flatten(on: req.eventLoop)
-        
-      }
+    let contacts = try req.content.decode([Contact.ReqRes].self)
+    let phoneNumbers: [String] = contacts.map { return $0.phoneNumber }
+
+    return User.query(on: req.db)
+      .with(\.$attachments)
+      .filter(\.$phoneNumber ~~ phoneNumbers)
+      .all().map { return $0.map { $0.response } }
   }
   
 }
